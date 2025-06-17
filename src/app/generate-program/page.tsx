ee7ba0690350d8e950f5ea2d55dfcb441e28aa6c
Fathima <fathimanaiha03@gmail.com>
2025-06-17 22:31:59 +0530

    initial commit

---
+++ src/app/generate-program/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GenerateProgramPage = () => {
+  return (
+    <div>
+        <h1>Generate Your Program</h1>
+        <p>Here you can create a personalized fitness program.</p>
+        
+    </div>
+  )
+}
+
+export default GenerateProgramPage
