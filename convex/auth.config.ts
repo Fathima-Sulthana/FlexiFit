10a9e55736cdac8ca7a7d546aec406534efcad6b
Fathima <fathimanaiha03@gmail.com>
2025-06-18 22:02:47 +0530

    vapi set up and convex set up done

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+// https://gentle-ladybug-79.clerk.accounts.dev
+
+export default {
+  providers: [
+    {
+      domain: "https://gentle-ladybug-79.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
